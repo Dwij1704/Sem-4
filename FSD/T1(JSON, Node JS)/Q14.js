04f966fcee091a830a2d9ce8fe1488168d71b418
Dwij Patel <dwijpatel1704@gmail.com>
2023-05-23 13:00:01 +0530

    T1

--- FSD/T1(JSON, Node JS)/Q14.js
+++ FSD/T1(JSON, Node JS)/Q14.js
@@ -1,6 +1,6 @@
 // Q14. Write a script of JSON array containing objects and display the same in console.
-var jsonArray = [
-  {
+var jsonArray = {
+  "objs":[{
     "name": "Dwij Patel",
     "age": 19,
     "city": "Ahmedabad"
@@ -14,8 +14,8 @@ var jsonArray = [
     "name": "Shubham Jain",
     "age": 19,
     "city": "Ahmedabad"
-  }
-];
+  }]
+};
 var jsonString = JSON.stringify(jsonArray);
 console.log(jsonString);
 //  JSON.stringify() function is used to convert the JSON array to a string before displaying it in the console.
