038eadea0a9588596cced6e6ad1d4da8863a26a6
Dwij Patel <dwijpatel1704@gmail.com>
2023-06-06 09:48:52 +0530

    T2 Lect-1

--- Python-2/T2/Chap-4 Web Programming Using Python (15%)/Lecture-1 06-06-23/Example1 TCP_Server.py
+++ Python-2/T2/Chap-4 Web Programming Using Python (15%)/Lecture-1 06-06-23/Example1 TCP_Server.py
@@ -2,30 +2,29 @@ import socket as sk
 host=sk.gethostname()
 port=8080
 addr=(host,port) #Creating tuple consisting host and port
-server_sk=sk.socket()
 #Creating socket
-server_sk.bind(addr)
+server_sk=sk.socket()
 #Binding socket to address
-server_sk.listen()
+server_sk.bind(addr)
 #Listening to socket
+server_sk.listen()
 print("Server is listening at port",port)
-#Printing message
 while True:
-    client_sk,client_addr=server_sk.accept()
     #Accepting connection from client
+    client_sk,client_addr=server_sk.accept()
     print("Client connected at",client_addr)
     while True:
+        #Receiving data from client
         data=client_sk.recv(1024)
         # data=client_sk.recv(1024).decode ->Converts recieved data from byte to readable
-        #Receiving data from client
         if not data:
             break
-        client_sk.send(data)
         #Sending data to client
-        client_sk.close()
+        client_sk.send(data)
         #Closing connection with client
+        client_sk.close()
         print("Client disconnected")
-        server_sk.close()
         #Closing connection with server
+        server_sk.close()
         break
     break
