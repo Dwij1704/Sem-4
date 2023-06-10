25d644cdf09e674873273ec4f0fd9f370bff0a61
Dwij Patel <dwijpatel1704@gmail.com>
2023-06-10 10:04:20 +0530

    T2 Lect-3

--- Python-2/T2/Chap-4 Web Programming Using Python (15%)/Lecture-3 10-06-23/Example8 API.py
+++ Python-2/T2/Chap-4 Web Programming Using Python (15%)/Lecture-3 10-06-23/Example8 API.py
@@ -1,7 +1,23 @@
-import requests, json
+import requests
 
 
 def getCoordinates(apiKey):
-    city = input("Enter a city")
+    city = input("Enter a city: ")
     req = "q=" + city + "&appid=" + apiKey
     url = "https://api.openweathermap.org/geo/1.0/direct?" + req
+    response = requests.get(url)
+    cordinates = response.json()
+    print(
+        "Longitude: ",
+        cordinates[0]["lon"],
+        "\nLatitude: ",
+        cordinates[0]["lat"],
+        "\nState: ",
+        cordinates[0]["state"],
+        "\nCountry: ",
+        cordinates[0]["country"],
+    )
+    # print(cordinates)
+
+
+getCoordinates("9ef8e21f1c5d7bea97133bd4ad1880dd")
