de6a8b03ee90aca6be17a9f9fc4cb3887907b766
Dwij Patel <dwijpatel1704@gmail.com>
2023-06-12 10:08:26 +0530

    T2 Lect-3

--- FSD/T2(Express JS)/Task-3.js
+++ FSD/T2(Express JS)/Task-3.js
@@ -4,25 +4,25 @@
 // to print warning message in RED color.
 const express = require('express');
 const path = require('path');
-const bp = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 app.use("/", (req, res) => {
     res.sendFile(path.join(__dirname,'Task-3.html'))
 })
-app.use(bp.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/check', (req, res, next) => {
-    // console.log('out')
+    console.log('out')
     res.set('Content-Type', 'text-html');
     if (req.body.username == 'admin') {
-        //console.log('if')
+        console.log('if')
         next();
     }
     else {
-      //  console.log('else')
+       console.log('else')
         res.send("<center><h1 style='color:red'>Warning⚠️</h1></center>")
     }
 }, (res,req)=>{
-   // console.log('next')
+   console.log('next')
     res.send('Welcome'+req.body.username)    
 })
 app.listen(8080)
