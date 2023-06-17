1d83ffd55e36df615d8d8dba70cc6d6e264aa349
Dwij Patel <dwijpatel1704@gmail.com>
2023-06-17 13:28:30 +0530

    T2 Chapter-4 Final Commit

---
+++ Python-2/T2/Chap-4 Web Programming Using Python (15%)/Lecture-6 17-06-23/Example 16 Offline webScrapping.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+
+soup = BeautifulSoup(
+    open(
+        "Web Scrapping\\top250.html",
+        "r",
+        encoding="utf-8",
+    )
+)
+nameYearList = soup.find_all("td", class_="titleColumn")
+ratingList = soup.find_all("td", class_="imdbRating")
+data = {"Title": [], "Year": [], "Rating": []}
+for i in range(len(nameYearList)):
+    data["Title"].append(nameYearList[i].find("a").text.strip())
+    data["Year"].append(nameYearList[i].find("span").text.strip()[1:-1])
+    data["Rating"].append(ratingList[i].find("strong").text.strip())
+df = pd.DataFrame(data)
+print(df)
