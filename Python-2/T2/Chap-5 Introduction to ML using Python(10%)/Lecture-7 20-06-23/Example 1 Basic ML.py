588eda3507b9ff902c06368e9bae9cc40195b029
Dwij Patel <dwijpatel1704@gmail.com>
2023-06-20 10:01:52 +0530

    T2 Lect-7

--- Python-2/T2/Chap-5 Introduction to ML using Python(10%)/Lecture-7 20-06-23/Example 1 Basic ML.py
+++ Python-2/T2/Chap-5 Introduction to ML using Python(10%)/Lecture-7 20-06-23/Example 1 Basic ML.py
@@ -4,3 +4,9 @@ import numpy as np
 
 df = pd.read_csv("ML csv's\Book1.csv")
 print(df.head())
+x = df.iloc[:, 0:1]
+y = df.iloc[:, -1]
+print(x)
+print(y)
+# print(type(x)) Dataframe
+# print(type(y)) Series
