896235a289d15319c3f3100d0678796f4543c596
Dwij Patel <dwijpatel1704@gmail.com>
2023-06-20 10:38:46 +0530

    T2 Lect-7

--- Python-2/T2/Chap-5 Introduction to ML using Python(10%)/Lecture-7 20-06-23/Example 1 Basic ML.py
+++ Python-2/T2/Chap-5 Introduction to ML using Python(10%)/Lecture-7 20-06-23/Example 1 Basic ML.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import sklearn as sk
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("ML csv's\Book1.csv")
 print(df.head())
@@ -10,3 +13,16 @@ print(x)
 print(y)
 # print(type(x)) Dataframe
 # print(type(y)) Series
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+print(x_train.shape)
+print(x_test.shape)
+print(y_train.shape)
+print(x_test.shape)
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+y_pred = lr.predict(x_test)
+d = pd.DataFrame({"Actual": y_test, "Predicted": y_pred})
+print(d)
+plt.scatter(df["cgpa"], df["package"])
+plt.plot(x_test, y_pred, color="red")
+plt.show()
