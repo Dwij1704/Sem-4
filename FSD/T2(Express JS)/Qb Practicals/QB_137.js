6cb23e3c24c11b9f019bc1928771ec49050b01aa
Dwij Patel <22002171220035@ljku.edu.in>
2023-06-28 07:42:35 +0530

    Exam commit

--- FSD/T2(Express JS)/Qb Practicals/QB_137.js
+++ FSD/T2(Express JS)/Qb Practicals/QB_137.js
@@ -1,1 +1,20 @@
 // Write a code snippet to configure the multer middleware to store uploaded files in a specific directory called "uploads".
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const app = express()
+app.use(express.static(__dirname,{index:'QB_137.html'}))
+const storageImage=multer.diskStorage({
+    destination:path.join(__dirname,'specific'),
+    filename: function (req, file, cb) {
+        cb(null,file.originalname)
+    }
+})
+var upload=multer({storage:storageImage})
+app.post('/process',upload.single('file'),(req,res)=>{
+    console.log(req)
+    console.log(req.files)
+    console.log(req.file)
+    res.send()
+})
+app.listen(8080)
---
+++ FSD/T2(Express JS)/Qb Practicals/QB_Chap5_23.js
@@ -0,0 +1,32 @@
+const exp=require('express')
+const multer=require('multer')
+const path=require('path')
+const app=exp()
+app.set('view engine','pug')
+app.get('/',(req,res)=>{
+    res.render(path.join(__dirname,'QB_Chap5_23.pug'))
+})
+var storageImage=multer.diskStorage({
+    destination:'specific',
+    filename:function(req,file,cb){
+        if(file.mimetype=='application/pdf'){
+            cb(null,file.originalname)
+        }
+        else{
+            cb(new Error('Only PDF Allowed'))
+        }
+    }
+})
+var upload=multer({storage:storageImage})
+app.post('/process',upload.single('myfile'),(err,req,res,next)=>{
+    if(err){
+        res.status(400).send(err.message)
+    }
+    else{
+        next()
+    }
+},
+(req,res)=>{
+    res.render(path.join(__dirname,'QB_Chap5_23Confirmation.pug'))
+})
+app.listen(8080)
