4eb6b8a7595a6855eeeacfb767cf9691ddb1728a
Dwij Patel <22002171220035@ljku.edu.in>
2023-07-17 10:45:55 +0530

    Lect-1

--- FSD/T3(React.js)/Chapter-6/my-react-app/src/App.js
+++ FSD/T3(React.js)/Chapter-6/my-react-app/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+
+function Lecture1Example1() {
+  return <div>Routing Example</div>;
+}
+
+function StudentPage() {
+  return <div>Student Page</div>;
+}
+
+function AboutPage() {
+  return <div>About Us</div>;
+}
+
+function MainContent() {
+  const location = useLocation();
+  if (location.pathname === '/' || location.pathname === '') {
+    return (
+    <>
+  <div className="container">
+    <div className="row justify-content-center align-items-center" style={{height:100+"vh"}}>
+      <div className="col-md-4">
+        <div className="card">
+          <div className="button-container">
+            <h2 className="heading">Lecture 1</h2>
+            <a className="btn btn-primary" href="lecture1/Example1">Example 1</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    </>
+      );
+  }
+
+  return null;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MainContent />
+      <Routes>
+        <Route path="/lecture1/Example1" element={<Lecture1Example1 />} />
+        {/* <Route path="/student" element={<StudentPage />} />
+        <Route path="/about" element={<AboutPage />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
