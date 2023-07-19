7e4424baa68a7ccff1c3ba6c32346ebc396bb7e8
Dwij Patel <22002171220035@ljku.edu.in>
2023-07-19 13:09:33 +0530

    Lect-2

--- FSD/T3(React.js)/Chapter-6/my-react-app/src/App.js
+++ FSD/T3(React.js)/Chapter-6/my-react-app/src/App.js
@@ -1,17 +1,8 @@
 import "./App.css";
+import Example2 from './Example 2/Example2';
+import Example3 from './Example3/Example3';
+import Example4 from "./Example4/Example4";
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-
-function Lecture1Example1() {
-  return <div>Routing Example</div>;
-}
-
-function StudentPage() {
-  return <div>Student Page</div>;
-}
-
-function AboutPage() {
-  return <div>About Us</div>;
-}
 
 function MainContent() {
   const location = useLocation();
@@ -24,7 +15,11 @@ function MainContent() {
         <div className="card">
           <div className="button-container">
             <h2 className="heading">Lecture 1</h2>
-            <a className="btn btn-primary" href="lecture1/Example1">Example 1</a>
+            <a className="btn btn-primary" href="lecture1/Example1">Example 1 Routing</a>
+            <h2 className="heading">Lecture 2</h2>
+            <a className="btn btn-primary" href="lecture2/Example2">Example 2 Module Import</a>
+            <a className="btn btn-primary" href="lecture2/Example3">Example 3 Task-1</a>
+            <a className="btn btn-primary" href="lecture2/Example4">Example 4 Props</a>
           </div>
         </div>
       </div>
@@ -33,15 +28,48 @@ function MainContent() {
     </>
       );
   }
-  return null
+
+  return null;
 }
 
+function Lecture1Example1() {
+  // Routing Example
+  return <div>Routing Example</div>;
+}
+function Lecture2Example2(){
+  // Module Import Example
+  return(
+    <div>
+      <Example2/>
+      {console.log("Hello World")}
+    </div>
+    )
+}
+function Lecture2Example3(){
+  // React Script to create a component which contains <h1>Name</h1>, <b>Date</b>, <p>Time</p>
+  return(
+    <>
+    <Example3/>
+    </>
+  )
+}
+function Lecture2Example4(){
+  // Props Example
+  return(
+    <>
+    <Example4 name="Dwij" marks="25"/>
+    </>
+  )
+}
 function App() {
   return (
     <BrowserRouter>
       <MainContent />
       <Routes>
         <Route path="/lecture1/Example1" element={<Lecture1Example1 />} />
+        <Route path="/lecture2/Example2" element={<Lecture2Example2 />} />
+        <Route path="/lecture2/Example3" element={<Lecture2Example3 />} />
+        <Route path="/lecture2/Example4" element={<Lecture2Example4 />} />
         {/* <Route path="/student" element={<StudentPage />} />
         <Route path="/about" element={<AboutPage />} /> */}
       </Routes>
---
+++ FSD/T3(React.js)/Chapter-6/my-react-app/src/Example 2/Example2.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Example2 = () => {  
+    return <h1>Module Import Example</h1>;
+}
+export default Example2
