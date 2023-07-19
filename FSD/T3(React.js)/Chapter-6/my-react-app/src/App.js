96d1c76b8698a179b31daa9de1a24999b3732b76
Dwij Patel <22002171220035@ljku.edu.in>
2023-07-19 13:43:51 +0530

    Lect-2

--- FSD/T3(React.js)/Chapter-6/my-react-app/src/App.js
+++ FSD/T3(React.js)/Chapter-6/my-react-app/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Example2 from './Example 2/Example2';
 import Example3 from './Example3/Example3';
 import Example4 from "./Example4/Example4";
+import Example5 from "./Example5/Example5";
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 
 function MainContent() {
@@ -20,6 +21,7 @@ function MainContent() {
             <a className="btn btn-primary" href="lecture2/Example2">Example 2 Module Import</a>
             <a className="btn btn-primary" href="lecture2/Example3">Example 3 Task-1</a>
             <a className="btn btn-primary" href="lecture2/Example4">Example 4 Props</a>
+            <a className="btn btn-primary" href="lecture2/Example5">Example 5 JSON Prop</a>
           </div>
         </div>
       </div>
@@ -61,6 +63,43 @@ function Lecture2Example4(){
     </>
   )
 }
+function Lecture2Example5(){
+  // JSON Prop Example
+  const car = {
+    img: "https://stimg.cardekho.com/images/carexteriorimages/360x240/Audi/Audi-R8/047.jpg",
+    name: "Audi",
+    model: "A8",
+    color: "Black",
+    price: "1.5 Crore",
+    engine: "V8",
+    power: "500hp",
+    torque: "700Nm",
+    transmission: "8-speed Automatic",
+    fuel: "Petrol",
+    topSpeed: "250kmph",
+    acceleration: "3.5sec",
+    seats: "4",
+    doors: "4",
+    length: "5.3m",
+    width: "2.1m",
+    height: "1.5m",
+    wheelbase: "3.1m",
+    groundClearance: "0.1m",
+    bootSpace: "500L",
+    fuelTankCapacity: "80L",
+    tyreSize: "255/45 R19",
+    wheelSize: "19 Inch",
+    wheelType: "Alloy",
+    mileageCity: "8kmpl",
+    mileageHighway: "12kmpl",
+    mileageOverall: "10kmpl",
+  }
+  return(
+    <>
+    <Example5 car={car}/>
+    </>
+    )
+}
 function App() {
   return (
     <BrowserRouter>
@@ -70,6 +109,7 @@ function App() {
         <Route path="/lecture2/Example2" element={<Lecture2Example2 />} />
         <Route path="/lecture2/Example3" element={<Lecture2Example3 />} />
         <Route path="/lecture2/Example4" element={<Lecture2Example4 />} />
+        <Route path="/lecture2/Example5" element={<Lecture2Example5 />} />
         {/* <Route path="/student" element={<StudentPage />} />
         <Route path="/about" element={<AboutPage />} /> */}
       </Routes>
