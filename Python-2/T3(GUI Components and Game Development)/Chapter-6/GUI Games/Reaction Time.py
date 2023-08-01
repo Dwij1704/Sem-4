f9f648538cd00c91d11054bcf29b88f4f5220e5b
Dwij Patel <22002171220035@ljku.edu.in>
2023-08-01 09:12:04 +0530

    Lect-6

---
+++ Python-2/T3(GUI Components and Game Development)/Chapter-6/GUI Games/Reaction Time.py
@@ -0,0 +1,26 @@
+import SimpleGUICS2Pygame.simpleguics2pygame as Simplegui
+frame=Simplegui.create_frame('Test',500,500,200) # Title, width, height, control width 
+time=0
+flag=0
+def timerHandler():
+    global time
+    time+=0.001
+def draw(canvas):
+    global time
+    canvas.draw_text(str(time),[250,250],20,'red')
+def start():
+    global flag,time
+    if flag==1:
+        timer.stop()
+        flag=0
+    else:
+        timer.start()
+        flag=1
+def restart():
+    global time
+    time=0
+timer=Simplegui.create_timer(1,timerHandler)
+frame.set_draw_handler(draw)
+frame.add_button('Start',start)
+frame.add_button('Restart',restart)
+frame.start()
